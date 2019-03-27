3aa161aecbcfeff5d4b92dd221b5c7a29ae16330
nykh2010 <xulingfeng@boe.com.cn>
2019-03-27 09:38:27 +0800

    将数据库存储修改为文件方式

--- epd_service/gateway.py
+++ epd_service/gateway.py
@@ -6,53 +6,114 @@ import downlink
 import threading
 import hashlib
 from epd_log import epdlog as LOG
+from task import EpdTask
+import os
 
 STEP_TIME = 20*60       # 任务间隔时间
 handler_interval = 10   # 10秒产生一次定时任务
 
 class Gateway(Config):
     # 网关参数
-    __authKey = None
-    __taskId = None
-    __taskStatus = None
-    __whiteListMD5 = None
+    __authKey = ""
+    __taskId = 0
+    __taskStatus = 0
+    __whiteListMD5 = ""
+    __whitelist = set()
     def __init__(self):
         super().__init__('gateway')
         # 启动定时任务
-        self.__whiteListMD5 = self.get_whitelist_md5()
+        self.__whiteListMD5 = self.get_whitelist()
         self.__taskId, self.__taskStatus = self.get_task_status()
         timer = threading.Timer(handler_interval, self.timer_handler)
         timer.start()
 
-    def get_whitelist_md5(self):
-        data = {
-            "table_name":"sql",
-            "sql_cmd":"select `device_id` from white_list;"
-        }
-        result = downlink.dl.send_service('database', data)
-        if result is None:
+    def get_whitelist(self):
+        try:
+            with open(self.white_list_url, 'r') as whitelistfile:
+                content = whitelistfile.read()
+                self.__whitelist = set(content.split('\n'))
+                hash_obj = hashlib.md5()
+                hash_obj.update(content.encode('utf-8'))
+                return str(hash_obj.hexdigest())
+        except Exception as e:
+            LOG.error("get white list failed: %s" % e.__str__())
             return ""
+
+    def create_whitelist(self, url, md5):
+        os.system("wget -c %s -O %s.tmp" % (url, self.white_list_url))
+        ret = self.check_whitelist_integrity(md5)
+        if not ret:
+            return False
         else:
-            md5_obj = hashlib.md5()
-            for res in result['result']:
-                md5_obj.update(("%s\n" % res['device_id']).encode('utf-8'))
-            md5_code = md5_obj.hexdigest()
-            LOG.info("white_list md5 code:%s", md5_code)
-            self.__whiteListMD5 = md5_code
-            return md5_code
+            os.system("mv %s.tmp %s" % (self.white_list_url, self.white_list_url))
+            self.get_whitelist()
+            return True
     
+    def check_whitelist_integrity(self, md5):
+        hash_obj = hashlib.md5()
+        with open("%s.tmp" % self.white_list_url, "rb") as whitelistfile:
+            hash_obj.update(whitelistfile.read())
+            hash_code = hash_obj.hexdigest()
+        if md5 != hash_code:
+            return False
+        return True
+    
+    def create_task(self, task_id, image_data_id, image_data_url,\
+            image_data_md5, iot_dev_list_md5, iot_dev_list_url, start_time, end_time):
+        os.system("wget -c %s -O %s.tmp" % (image_data_url, self.data_url))
+        os.system("wget -c %s -O %s.tmp" % (iot_dev_list_url, self.execute_url))
+        ret = self.check_task_integrity(image_data_md5, iot_dev_list_md5)
+        if not ret:
+            return False
+
+        # 0-none 1-sleep 2-ready 3-run 4-finish 5-suspend
+        if self.__taskId is None or (task_id != self.__taskId and self.__taskStatus in ('','none','sleep','finish','suspend')):
+            # 保存任务状态
+            epd_task = EpdTask()
+            epd_task.set_item('task_id', str(task_id))
+            epd_task.set_item('image_data_id', str(image_data_id))
+            epd_task.set_item('image_data_url', image_data_url)
+            epd_task.set_item('start_time', start_time)
+            epd_task.set_item('end_time', end_time)
+            epd_task.set_item('task_status', 'sleep')
+            epd_task.save()
+            # 保存待更新列表
+            os.system("mv %s.tmp %s" % (self.data_url, self.data_url))
+            os.system("mv %s.tmp %s" % (self.execute_url, self.execute_url))
+            self.__taskId, self.__taskStatus = self.get_task_status()     # 更新任务状态
+            return True
+        else:
+            return False
+
+    def cancel_task(self, task_id):
+        if self.__taskId != task_id:
+            return False
+        epd_task = EpdTask()
+        epd_task.set_item('task_status', 'none')
+        epd_task.set_item('task_id', '0')
+        epd_task.save()
+        self.__taskId, self.__taskStatus = self.get_task_status()     # 更新任务状态
+        return True
+
+    def check_task_integrity(self, image_data_md5, iot_dev_list_md5):
+        hash_obj = hashlib.md5()
+        with open("%s.tmp" % self.data_url, "rb") as datafile:
+            hash_obj.update(datafile.read())
+            hash_code = hash_obj.hexdigest()
+        if image_data_md5 != hash_code:
+            return False
+        
+        hash_obj = hashlib.md5()
+        with open("%s.tmp" % self.execute_url, "rb") as devfile:
+            hash_obj.update(devfile.read())
+            hash_code = hash_obj.hexdigest()
+        if iot_dev_list_md5 != hash_code:
+            return False
+        return True
+
     def get_task_status(self):
-        data = {
-            'table_name': 'sql',
-            'sql_cmd': 'select `task_id`,`status` from task;'
-        }
-        result = downlink.dl.send_service('database', data)
-        LOG.info('get task status result: %s', result['status'])
-        if result['result']:
-            self.__taskId = result['task_id']
-            self.__taskStatus = result['status']
-            return result['task_id'], result['status']
-        return 0, 0
+        epd_task = EpdTask()
+        return int(epd_task.task_id), epd_task.task_status
     
     def get_auth_key(self):
         # 从文件中获取auth_key
--- epd_service/protocol.py
+++ epd_service/protocol.py
@@ -34,13 +34,13 @@ class HeartRequest(Handle):
         #     "device_id":request['device_id'],
         #     "time":time_str
         # }
-        sql_cmd = "update `white_list` set `last_connect_time`='%s' where `device_id`='%s';" % (time_str, request['device_id'])
-        LOG.info("query database: %s", sql_cmd)
-        data = {
-            "table_name":"sql",
-            "sql_cmd": sql_cmd
-        }
-        dl.send_service('database', data)
+        # sql_cmd = "update `white_list` set `last_connect_time`='%s' where `device_id`='%s';" % (time_str, request['device_id'])
+        # LOG.info("query database: %s", sql_cmd)
+        # data = {
+        #     "table_name":"sql",
+        #     "sql_cmd": sql_cmd
+        # }
+        # dl.send_service('database', data)
         self.upload(request)
     
     def upload(self, data):
@@ -66,38 +66,38 @@ class TaskRequest(Handle):
     def func(self, data):
         task_id = data['task_id']
         status = data['status']
-        success_list = data.get('success_list', None)
-        fail_list = data.get('fail_list', None)
-        sql_cmd = "update `task` set `status`='%d' where `task_id`='%s';" % (status, task_id)
-        request = {
-            "table_name":"sql",
-            "sql_cmd": sql_cmd
-        }
-        LOG.info("query database:%s", sql_cmd)
-        ret = dl.send_service('database', request)
+        success_list = data.get('success_list', [])
+        fail_list = data.get('fail_list', [])
+        # sql_cmd = "update `task` set `status`='%d' where `task_id`='%s';" % (status, task_id)
+        # request = {
+        #     "table_name":"sql",
+        #     "sql_cmd": sql_cmd
+        # }
+        # LOG.info("query database:%s", sql_cmd)
+        # ret = dl.send_service('database', request)
         
-        if success_list:
-            chunk = ""
-            for success in success_list:
-                chunk += "`device_id`=%s or" % success
-            chunk = chunk[:-2]
-            # LOG.info(chunk)
-            request = {
-                "table_name":"sql",
-                "sql_cmd":"update `execute` set `status`=2 where %s;" % chunk
-            }
-            dl.send_service('database', request)
-        if fail_list:
-            chunk = ""
-            for fail in fail_list:
-                chunk += "`device_id`=%s or" % fail
-            chunk = chunk[:-2]
-            LOG.info(chunk)
-            request = {
-                "table_name":"sql",
-                "sql_cmd":"update `execute` set `status`=3 where %s;" % chunk
-            }
-            dl.send_service('database', request)
+        # if success_list:
+        #     chunk = ""
+        #     for success in success_list:
+        #         chunk += "`device_id`=%s or" % success
+        #     chunk = chunk[:-2]
+        #     # LOG.info(chunk)
+        #     request = {
+        #         "table_name":"sql",
+        #         "sql_cmd":"update `execute` set `status`=2 where %s;" % chunk
+        #     }
+        #     dl.send_service('database', request)
+        # if fail_list:
+        #     chunk = ""
+        #     for fail in fail_list:
+        #         chunk += "`device_id`=%s or" % fail
+        #     chunk = chunk[:-2]
+        #     LOG.info(chunk)
+        #     request = {
+        #         "table_name":"sql",
+        #         "sql_cmd":"update `execute` set `status`=3 where %s;" % chunk
+        #     }
+        #     dl.send_service('database', request)
         # 上传执行状态
         send_data = {
             "task_id":task_id,
@@ -135,106 +135,39 @@ class TaskApp(RequestHandler):
             request = json.loads(request)
             body = request['d']
             if cmd == 'create':
-                data = {
-                    "table_name":"sql",
-                    "sql_cmd":"select `status` from `task` where `task_id`=%s" % body['task_id']
-                }
-                resp = dl.send_service('database', data)
-                # print(resp)
-                if resp['status'] == 200:
-                    task_status = None
-                    if resp['result']:
-                        record = resp['result'][0]
-                        task_status = int(record['status'])
-                    if task_status in (0,1,4,5) or task_status is None:
-                        # 任务处于可覆盖状态
-                        if task_status is None:
-                            # 如果查询结果位空，则需要先添加记录
-                            data = {
-                                "table_name":"sql",
-                                "sql_cmd":"insert into `task`(`task_id`,`data_id`,`data_url`,`start_time`, `end_time`, `status`) \
-                                    VALUES ('%s','%s','%s','%s', '%s', %d); " % \
-                                    (body['task_id'], body['image_data_id'], body['image_data_url'], body['start_time'], body['end_time'], 1)
-                            }
-                        else:
-                            data = {
-                                "table_name":"sql",
-                                "sql_cmd":"update `task` set (`data_id`='%s', `data_url`='%s', `start_time`='%s', `end_time`='%s', `status`=1) where `task_id`='%s';" %\
-                                    (body['image_data_id'], body['image_data_url'], body['start_time'], body['end_time'], body['task_id'])
-                            }
-                        result = dl.send_service('database', data)
-                        if result['status'] != 200:
-                            raise HTTPError(500, log_message=result['msg'])
-                        
-                        # 生成执行表
-                        task_id = body['task_id']
-                        iot_dev_list_url = body['iot_dev_list_url']
-                        os.system('wget -c %s -O iot_dev_list' % iot_dev_list_url)
-                        hash_obj = hashlib.md5()
-                        with open('iot_dev_list', 'rb') as f:
-                            dev_list = f.readlines()
-                            chunk = ""
-                            for dev in dev_list:
-                                chunk += '(%s, %s, 1),' % (dev, task_id)
-                                hash_obj.update(dev)
-                            hash_code = hash_obj.hexdigest()
-                        if body['iot_dev_list_md5'] != hash_code:
-                            raise HTTPError(400, "iot dev list md5 check failed")
-                        
-                        data = {
-                            "table_name":"sql",
-                            "sql_cmd":"delete from `execute`;\
-                                insert into `execute`(`device_id`,`task_id`,`status`) values \
-                                %s;" % chunk
-                        }
-                        result = dl.send_service('database', data)
-                        if result['status'] != 200:
-                            raise HTTPError(500, log_message=result['msg'])
-                        # 检测文件是否存在
-                        if os.path.exists('/media/%s.bin' % str(body['image_data_id'])):
-                            pass
-                        else:
-                            ret_code = os.system("wget -c %s -O /media/%s.bin" % (body['image_data_url'], str(body['image_data_id'])))
-                            LOG.info('task data download complete, ret code:%d', ret_code)
-                            with open('/media/%s.bin' % body['image_data_id'], 'rb') as f:
-                                hash_obj = hashlib.md5()
-                                while True:
-                                    content = f.read(1024)
-                                    if not content:
-                                        break
-                                    hash_obj.update(content)
-                                hash_code = hash_obj.hexdigest()
-                                if body['image_data_md5'] != hash_code:
-                                    raise HTTPError(400, "data file md5 check failed")
-                        data = {
-                            "cmd":"task",
-                            "method":"create",
-                            "task_id":body['task_id'],
-                            "status":1
-                        }
-                        dl.send_service('serial', data)
-                        # raise HTTPError(200)
-                        status = 'ok'
-                        msg = "task_id %s create success" % body['task_id']
-                    else:
-                        # 不可创建任务
-                        LOG.info("task could not create, task status: %d", task_status)
-                        status = 'failed'
-                        msg = "task_id %s create not allowed" % body['task_id']
-                    # 上传命令处理结果
-                    upload = uplink.Upload()
-                    resp = {
-                        "id": request['id'],
-                        "from": request['from'],
-                        "status": status,
-                        "command": request['command'],
-                        "d":{
-                            "code":"task_status",
-                            "msg":msg
-                        }
+                ret = gw.create_task(body['task_id'], body['image_data_id'], body['image_data_url'], \
+                    body['image_data_md5'], body['iot_dev_list_md5'], body['iot_dev_list_url'], \
+                    body['start_time'], body['end_time'])
+                if ret:
+                    data = {
+                        "cmd":"task",
+                        "method":"create",
+                        "task_id":body['task_id'],
+                        "status":1
                     }
-                    gw.get_task_status()
-                    upload.send(resp, topic='dma/cmd/resp')
+                    dl.send_service('serial', data)
+                    # raise HTTPError(200)
+                    status = 'ok'
+                    msg = "task_id %s create success" % body['task_id']
+                else:
+                    # 不可创建任务
+                    LOG.info("task could not create")
+                    status = 'failed'
+                    msg = "task_id %s create not allowed" % body['task_id']
+                    # 上传命令处理结果
+                upload = uplink.Upload()
+                resp = {
+                    "id": request['id'],
+                    "from": request['from'],
+                    "status": status,
+                    "command": request['command'],
+                    "d":{
+                        "code":"task_status",
+                        "msg":msg
+                    }
+                }
+                # gw.get_task_status()
+                upload.send(resp, topic='dma/cmd/resp')
             elif cmd == 'cancel':
                 # cancel task
                 data = {
@@ -244,22 +177,29 @@ class TaskApp(RequestHandler):
                 }
                 dl.send_service('serial', data)
                 
-                data = {
-                    "table_name":"sql",
-                    "sql_cmd":"delete from task where task_id='%s';" \
-                        % body['task_id']
-                }
-                dl.send_service('database', data)
+                ret = gw.cancel_task(body['task_id'])
+                if ret:
+                    status = 'ok'
+                    result = {'result':'ok'}
+                else:
+                    status = 'err'
+                    result = {'code':'cancel_failed', 'msg':'task not found'}
+                
+
+                # data = {
+                #     "table_name":"sql",
+                #     "sql_cmd":"delete from task where task_id='%s';" \
+                #         % body['task_id']
+                # }
+                # dl.send_service('database', data)
                 # 上传处理结果
                 upload = uplink.Upload()
                 resp = {
                     "id": request['id'],
                     "from": request['from'],
-                    "status": "ok",
+                    "status": status,
                     "command": request['command'],
-                    "d":{
-                        "result":"ok"
-                    }
+                    "d":result
                 }
                 upload.send(resp, topic='dma/cmd/resp')
             elif cmd == 'confirm':
@@ -274,6 +214,7 @@ class TaskApp(RequestHandler):
                 raise HTTPError(404)
         except Exception as e:
             self.write(e.__str__())
+            LOG.error("%s" % e.__str__())
 
 class RadioApp(RequestHandler):
     def post(self, cmd):
@@ -305,32 +246,11 @@ class GatewayApp(RequestHandler):
                 pass
                 # gateway.set_group(data.get('gateway_list', None))
             elif cmd == 'white_list':
-                white_list_url = body['url']
-                white_list_md5 = body['md5']
-                LOG.info("get white list update request")
-                os.system('wget -c %s -O white_list' % white_list_url)
-                hash_obj = hashlib.md5()
-                with open('white_list', 'rb') as white_list_file:
-                    chunk = ""
-                    for line in white_list_file.readlines():
-                        hash_obj.update(line)
-                        chunk += "('%s')," % (line[:-1].decode('utf-8'))
-                hash_code = hash_obj.hexdigest()
-
-                if hash_code != white_list_md5:
-                    LOG.error('white list file md5 check failed')
-                    raise EpdException(code=400, message="md5 check failed")
-                else:
-                    data = {
-                        "table_name": "sql",
-                        "sql_cmd": "delete from `white_list`;\
-                            insert into white_list(`device_id`) values %s;" % chunk[:-1]
-                    }
-                    dl.send_service('database', data)
-                    gw.get_whitelist_md5()
-                    LOG.info('update database, white_list table')
+                ret = gw.create_whitelist(body['url'], body['md5'])
+                if ret:
+                    LOG.info("white list create success")
                     resp_status = "ok"
-                    resp_data = {"result":"ok"}
+                    resp_data = {"result": "ok"}
             elif cmd == 'check_code':
                 gw.set_auth_key(body['check_code'])
                 resp_status = "ok"
